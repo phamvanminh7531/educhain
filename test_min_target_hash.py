d8b964a66d35c7f253dd7defca43036898aa6c93
Minh Pham Van <phamvanminh@Minhs-MacBook-Pro.local>
2024-05-14 23:45:26 +0700

    test CICD 1

--- test_min_target_hash.py
+++ test_min_target_hash.py
@@ -4,7 +4,7 @@ import json
 from core.utils import calculate_hash
 from datetime import datetime
 
-target_hash = '0x1f3fe0c0000000000000000000000000000000000000000000000000000'
+target_hash = '0xc6ff390000000000000000000000000000000000000000000000000000'
 diff = int(target_hash, 16)
 
 
